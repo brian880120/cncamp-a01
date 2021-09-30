29672556803c0168cc85c4c381612f022f712b8a
Bo Dai (LCL) <bo.dai@loblaw.ca>
2021-09-29 21:36:20 -0400

    init commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"cncamp_a01/handler"
+	"cncamp_a01/middleware"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+	r.HandleFunc("/healthz", handler.Healthz)
+	r.HandleFunc("/notFound", handler.NotFound)
+	r.Use(middleware.CORS)
+	r.Use(middleware.ResponseHeader)
+	r.Use(middleware.Log)
+	http.ListenAndServe(":8090", r)
+}
